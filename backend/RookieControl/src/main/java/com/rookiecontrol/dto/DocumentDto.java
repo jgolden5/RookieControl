997bb5768e18a366e74a30bed6f9f577a84fa413
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-12-29 23:08:33 -0700

    added DocumentDto

---
+++ backend/RookieControl/src/main/java/com/rookiecontrol/dto/DocumentDto.java
@@ -0,0 +1,32 @@
+package com.rookiecontrol.dto;
+
+public class DocumentDto {
+  private String documentCode;
+  private String title;
+  private String fileType;
+
+  public String getDocumentCode() {
+    return documentCode;
+  }
+
+  public void setDocumentCode(String documentCode) {
+    this.documentCode = documentCode;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getFileType() {
+    return fileType;
+  }
+
+  public void setFileType(String fileType) {
+    this.fileType = fileType;
+  }
+  //constructors, getters, and setters will go here
+}
